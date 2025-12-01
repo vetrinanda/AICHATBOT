c176e831a720fbe63a7c1017979a196be8b1b3ff
Nanda Kumar <nk9219382@gmail.com>
2025-12-01 22:43:13 +0530

    feat: Add RAG agent implementation with document loading and retrieval capabilities

--- agents/rag.py
+++ agents/rag.py
@@ -1,26 +1,42 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, GoogleGenerativeAI
 from sklearn.metrics.pairwise import cosine_similarity
 from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_classic.chains import RetrievalQA
 import os
 load_dotenv()
 
+loader=TextLoader('input.txt')
+documents=loader.load()
+
+text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+docs=text_splitter.split_documents(documents)
 
 embeddings = GoogleGenerativeAIEmbeddings(
     google_api_key=os.getenv("API_KEY"),
     model="models/gemini-embedding-exp-03-07")
 
-text="Hi My name is Anand"
 
-result=embeddings.embed_query(text)
+vectorstore=FAISS.from_documents(docs, embeddings)
 
-docs=[
-    "Hello world",
-    "Hi there",
-    "Greetings planet",
-    "Salutations earth"
-]
-result1=embeddings.embed_documents(docs)
+retriever=vectorstore.as_retriever()
 
-similarity=cosine_similarity([result], result1)
 
-print(similarity)
+model=GoogleGenerativeAI(
+    google_api_key=os.getenv("API_KEY"),
+    model="gemini-2.5-flash"
+)
+
+
+chain=RetrievalQA.from_chain_type(
+    llm=model,
+    retriever=retriever
+)
+
+query="Explain the key concepts discussed about AI?"
+response=chain.invoke(query)
+
+print("Response:")
+print(response["result"])
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from agents.chat import simple_chat
+from agents.rag import rag_chat
 from pydantic import BaseModel
 
 app = FastAPI()
