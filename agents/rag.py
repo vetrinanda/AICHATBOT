738178e1b80069b95454c8e31a2d6ac21f5cacaf
Nanda Kumar <nk9219382@gmail.com>
2025-12-01 21:52:04 +0530

    feat: Implement RAG agent with Google Generative AI embeddings and cosine similarity calculation

--- agents/rag.py
+++ agents/rag.py
@@ -1,7 +1,26 @@
-from langchain_litellm import ChatLiteLLM
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from sklearn.metrics.pairwise import cosine_similarity
 from dotenv import load_dotenv
 import os
 load_dotenv()
-#i will work on it 
 
 
+embeddings = GoogleGenerativeAIEmbeddings(
+    google_api_key=os.getenv("API_KEY"),
+    model="models/gemini-embedding-exp-03-07")
+
+text="Hi My name is Anand"
+
+result=embeddings.embed_query(text)
+
+docs=[
+    "Hello world",
+    "Hi there",
+    "Greetings planet",
+    "Salutations earth"
+]
+result1=embeddings.embed_documents(docs)
+
+similarity=cosine_similarity([result], result1)
+
+print(similarity)
